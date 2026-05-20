a99cc16d5b2a1bc117b6d6f68546e435f982c718
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2026-05-20 15:57:52 -0500

    feat: finish iterative fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,12 +1,23 @@
-const fibs = (n) => {};
+const fibs = (n) => {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+
+  const arr = [0, 1];
+  for (let i = 2; i < n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr;
+};
+
 const fibsRec = (n) => {
-  if (n <= 0) {
-    return 0;
-  }
-  if (n === 1) {
-    return 1;
-  }
-  return fibsRec(n - 2) + fibsRec(n - 1);
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const prev = fibsRec(n - 1);
+  const next = prev[prev.length - 1] + prev[prev.length - 2];
+
+  return [...prev, next];
 };
 
 module.exports = {
--- fibonacci.test.js
+++ fibonacci.test.js
@@ -1,17 +1,33 @@
 const { fibs, fibsRec } = require("./fibonacci");
 
-test("Gets first value of fibonacci", () => {
-  expect(fibsRec(1)).toEqual(1);
+test("Gets first value of the iterative fibonacci", () => {
+  expect(fibs(1)).toEqual([0]);
 });
 
-test("Gets second value of fibonacci", () => {
-  expect(fibsRec(2)).toEqual(1);
+test("Gets second value of the iterative fibonacci", () => {
+  expect(fibs(2)).toEqual([0, 1]);
 });
 
-test("Gets third value of fibonacci", () => {
-  expect(fibsRec(3)).toEqual(2);
+test("Gets third value of the iterative fibonacci", () => {
+  expect(fibs(3)).toEqual([0, 1, 1]);
 });
 
-test("Gets sixth value of fibonacci", () => {
-  expect(fibsRec(6)).toEqual(8);
+test("Gets sixth value of the iterative fibonacci", () => {
+  expect(fibs(6)).toEqual([0, 1, 1, 2, 3, 5]);
+});
+
+test("Gets first value of the recurisve fibonacci", () => {
+  expect(fibsRec(1)).toEqual([0]);
+});
+
+test("Gets second value of the recurisve fibonacci", () => {
+  expect(fibsRec(2)).toEqual([0, 1]);
+});
+
+test("Gets third value of the recurisve fibonacci", () => {
+  expect(fibsRec(3)).toEqual([0, 1, 1]);
+});
+
+test("Gets sixth value of the recurisve fibonacci", () => {
+  expect(fibsRec(6)).toEqual([0, 1, 1, 2, 3, 5]);
 });
